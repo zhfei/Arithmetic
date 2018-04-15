ec3c33070f25bdbf9a06639294befe8f0fcbcdaf
周飞 <zhoufei@zhoufeideMacBook-Pro.local>
2018-04-15 13:17:20 +0800

    add:添加忽略文件

---
+++ SortAlgorithm/SortAlgorithm/BubblingSort.h
@@ -0,0 +1,19 @@
+//
+//  BubblingSort.h
+//  ArithmeticDemo
+//
+//  Created by 周飞 on 18/3/22.
+//  Copyright © 2018年 周飞. All rights reserved.
+//
+
+#ifndef BubblingSort_h
+#define BubblingSort_h
+
+#include <stdio.h>
+
+void bubblingSort1(int array[], int length);
+
+void bubblingSort2(int array[], int length);
+
+
+#endif /* BubblingSort_h */
