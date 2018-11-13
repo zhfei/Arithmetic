004762cf972fd42bc26f5c0e8c0537d5ec712cc0
周飞 <1174977961@qq.com>
2018-11-13 23:59:09 +0800

    add:添加单向链表删除方法

--- SortAlgorithm/DataStructure/线性表/链式存储结构/LinkList.c
+++ SortAlgorithm/DataStructure/线性表/链式存储结构/LinkList.c
@@ -67,6 +67,31 @@ Status insertElement(pNode L,int index, ElemteType e) {
     return 1;
 }
 
+Status removeElement(pNode L,int index, ElemteType *e) {
+    
+    pNode p = L->next;
+    int num = 2;//获得要插入位置的前一个节点
+    while (p && num < index) {
+        num++;
+        p = p->next;
+    }
+    
+    if (num < index) {
+        printf("不存在。。");
+    } else {
+        printf("删除前，index节点:%d,的值：%d\n",index,p->next->data);
+        pNode delNode = p->next;
+        p->next = delNode->next;
+        free(delNode);
+        
+        printf("删除后，index节点:%d,的值：%d\n",index,p->next->data);
+    }
+    
+    
+    
+    return 1;
+}
+
 pNode createLinkList(int count) {
     pNode header = (pNode)malloc(sizeof(Node));
     pNode tailer = header;
