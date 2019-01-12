7256569fa554d19b6c1cb38daac3ddcedc827301
周飞 <1174977961@qq.com>
2019-01-12 23:49:02 +0800

    add:添加冒泡排序test

---
+++ SortAlgorithm/Test/Test1.c
@@ -0,0 +1,27 @@
+//
+//  Test1.c
+//  SortAlgorithm
+//
+//  Created by 周飞 on 2019/1/12.
+//  Copyright © 2019年 周飞. All rights reserved.
+//
+//  两个for循环
+//  第一个从0->count-1 变量为i,条件i<count-1
+//  第二个从count-1->i 变量为j,条件j>i
+
+
+#include "Test1.h"
+
+void bubblingSort(int array[], int count) {
+    for (int i = 0; i < count; i++) {
+        for (int j = count - 1; j > i; j--) {
+            if (array[j] < array[j-1]) {
+                int temp = array[j];
+                array[j] = array[j-1];
+                array[j-1] = temp;
+            }
+        }
+    }
+    
+    
+}
--- SortAlgorithm/Test/Test1.h
+++ SortAlgorithm/Test/Test1.h
@@ -1,17 +1,14 @@
 //
-//  Test.h
+//  Test1.h
 //  SortAlgorithm
 //
 //  Created by 周飞 on 2019/1/12.
 //  Copyright © 2019年 周飞. All rights reserved.
 //
 
-#ifndef Test_h
-#define Test_h
+#ifndef Test1_h
+#define Test1_h
 
-/*
- 1.快速排序
- 2.冒泡排序
- */
+#include <stdio.h>
 
-#endif /* Test_h */
+#endif /* Test1_h */
