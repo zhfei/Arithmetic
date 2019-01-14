17ee6fea8fe1c80b733f72afb6b7883080d0e8d2
周飞 <1174977961@qq.com>
2019-01-14 23:57:05 +0800

    add:添加二叉树反转test

---
+++ SortAlgorithm/Test/Test7.c
@@ -0,0 +1,34 @@
+//
+//  Test7.c
+//  SortAlgorithm
+//
+//  Created by zhoufei on 2019/1/14.
+//  Copyright © 2019年 周飞. All rights reserved.
+//
+//  反转二叉树
+
+#include "Test7.h"
+#include <stdlib.h>
+
+typedef struct Node {
+    int value;
+    struct Node *left;
+    struct Node *reight;
+}Node,*pNode;
+
+void reversalTree(pNode tree) {
+    if (tree->left == NULL) {
+        return;
+    }
+    if (tree->reight == NULL) {
+        return;
+    }
+    
+    pNode left = tree->left;
+    pNode reight = tree->reight;
+    tree->left = reight;
+    tree->reight = left;
+    
+    reversalTree(tree->left);
+    reversalTree(tree->reight);
+}
---
+++ SortAlgorithm/Test/Test7.h
@@ -0,0 +1,14 @@
+//
+//  Test7.h
+//  SortAlgorithm
+//
+//  Created by zhoufei on 2019/1/14.
+//  Copyright © 2019年 周飞. All rights reserved.
+//
+
+#ifndef Test7_h
+#define Test7_h
+
+#include <stdio.h>
+
+#endif /* Test7_h */
