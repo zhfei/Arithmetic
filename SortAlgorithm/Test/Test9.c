e35902a5f9a3a776472416e7ab254e76fadd1b75
周飞 <1174977961@qq.com>
2019-01-15 23:57:47 +0800

    other:完善归并test

--- SortAlgorithm/Test/Test9.c
+++ SortAlgorithm/Test/Test9.c
@@ -28,8 +28,31 @@ void reversalStr2(char str[]) {
     printf("%s",str);
 }
 //归并排序
-
+static int arrayLength = 10;
 void mergerAction2(int array1[], int length1, int array2[], int length2) {
+    int i,j,k,m;
+    int tmp[arrayLength];
+    i=j=k=m=0;
+    
+    while (i<length1 && j<length2) {
+        if (array1[i] < array2[j]) {
+            tmp[k++] = array1[i++];
+        } else {
+            tmp[k++] = array2[j++];
+        }
+    }
+    
+    while (i < length1) {
+        tmp[k++] = array1[i++];
+    }
+    
+    while (j < length2) {
+        tmp[k++] = array2[j++];
+    }
+    
+//    for (<#initialization#>; <#condition#>; <#increment#>) {
+//        <#statements#>
+//    }
     
 }
 
