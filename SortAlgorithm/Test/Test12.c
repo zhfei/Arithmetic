d3b0b21d3bfbd5ca912101baf46c12e59a10e1fa
周飞 <1174977961@qq.com>
2019-01-18 21:22:10 +0800

    add:添加最大公约数test

--- SortAlgorithm/Test/Test12.c
+++ SortAlgorithm/Test/Test12.c
@@ -7,21 +7,20 @@
 //
 
 #include "Test12.h"
-
 // 不用中间变量，用两种方法交换两个数的值
+//+,-法交换
 void exchangeNum1(int a, int b) {
     // a = 2,  b = 3
     a = a + b; //a = 5
     b = a - b; //b = 5 - 3 = 2
     a = a - b; //a = 5 - 2 = 3;
 }
-//对应上面的+,-
+//*，/法交换，对应上面的+,-
 void exchangeNum2(int a, int b) {
     a = a * b;
     b = a / b;
     a = a / b;
 }
-
 //异或：对应的两位 相同为0， 不同为1
 void exchangeNum3(int a, int b) {
     //a = 1 = 001
@@ -31,3 +30,19 @@ void exchangeNum3(int a, int b) {
     b = a ^ b; //b= 001
     a = a ^ b; //a= 011
 }
+
+
+//-------------------------------------------------------
+//求两个数的最大公约数
+//扩展：最小公倍数 = (a * b)/最大公约数
+void commonDivisor3(int a, int b) {
+    int flag = a < b?:b;
+    int num = 1;
+    int result = 0;
+    while (num++ <= flag) {
+        if (a % num == 0 && b % num == 0) {
+            result = num;
+        }
+    }
+}
+
