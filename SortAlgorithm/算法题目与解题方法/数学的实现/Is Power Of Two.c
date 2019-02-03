fa002d2a8601bd54001c1ed6d2a3c79d4fd07476
周飞 <1174977961@qq.com>
2019-02-03 23:58:17 +0800

    add: 添加是否为2的幂数

---
+++ SortAlgorithm/算法题目与解题方法/数学的实现/Is Power Of Two.c
@@ -0,0 +1,9 @@
+//
+//  Is Power Of Two.c
+//  SortAlgorithm
+//
+//  Created by 周飞 on 2019/2/3.
+//  Copyright © 2019年 周飞. All rights reserved.
+//
+
+#include "Is Power Of Two.h"
---
+++ SortAlgorithm/算法题目与解题方法/数学的实现/Is Power Of Two.h
@@ -0,0 +1,14 @@
+//
+//  Is Power Of Two.h
+//  SortAlgorithm
+//
+//  Created by 周飞 on 2019/2/3.
+//  Copyright © 2019年 周飞. All rights reserved.
+//
+
+#ifndef Is_Power_Of_Two_h
+#define Is_Power_Of_Two_h
+
+#include <stdio.h>
+
+#endif /* Is_Power_Of_Two_h */
