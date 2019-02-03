d393a89ebee1112eb564fa5bb398f90e91014f12
周飞 <1174977961@qq.com>
2019-02-03 23:46:34 +0800

    add: 添加二分查找

--- SortAlgorithm/算法题目与解题方法/数学的实现/Binary Search.c
+++ SortAlgorithm/算法题目与解题方法/数学的实现/Binary Search.c
@@ -5,5 +5,29 @@
 //  Created by 周飞 on 2019/2/3.
 //  Copyright © 2019年 周飞. All rights reserved.
 //
+// 二分查找
+//  从有序数组中查找一个数字位置
 
 #include <stdio.h>
+
+int BinarySearch(int array[], int num) {
+    int first = 0;
+    int last = sizeof(array)-1;
+    
+    while (first < last) {
+        int mid = (first + last)/2;
+        
+        if (array[mid] == num) {
+            return mid;
+        }
+        
+        if (num < array[mid]) {
+            last = mid - 1;
+        }
+        
+        if (num > array[mid]) {
+            first = mid + 1;
+        }
+    }
+    return first;
+}
