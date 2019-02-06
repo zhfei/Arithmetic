8b85f7cf41a5e87058f73a6c7b155c49562691d1
周飞 <1174977961@qq.com>
2019-02-06 23:55:01 +0800

    add: 添加数组生成工具

---
+++ SortAlgorithm/算法题目与解题方法/算法实现题/ArrayUtil.cpp
@@ -0,0 +1,9 @@
+//
+//  ArrayUtil.cpp
+//  SortAlgorithm
+//
+//  Created by 周飞 on 2019/2/6.
+//  Copyright © 2019年 周飞. All rights reserved.
+//
+
+#include "ArrayUtil.hpp"
---
+++ SortAlgorithm/算法题目与解题方法/算法实现题/ArrayUtil.hpp
@@ -0,0 +1,24 @@
+//
+//  ArrayUtil.hpp
+//  SortAlgorithm
+//
+//  Created by 周飞 on 2019/2/6.
+//  Copyright © 2019年 周飞. All rights reserved.
+//
+
+#ifndef ArrayUtil_hpp
+#define ArrayUtil_hpp
+
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+
+using namespace std;
+
+namespace ArrayUtil {
+    
+}
+
+
+#endif /* ArrayUtil_hpp */
