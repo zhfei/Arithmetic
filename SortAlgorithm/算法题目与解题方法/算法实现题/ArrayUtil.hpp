3365dde40ce274d329b4bb8518baa02b15055f0b
周飞 <1174977961@qq.com>
2019-02-06 23:58:59 +0800

    add: 添加数组生成方法

--- SortAlgorithm/算法题目与解题方法/算法实现题/ArrayUtil.hpp
+++ SortAlgorithm/算法题目与解题方法/算法实现题/ArrayUtil.hpp
@@ -17,7 +17,16 @@
 using namespace std;
 
 namespace ArrayUtil {
-    
+    int *generateRandomArray(int n, int rangeL, int rangeR) {
+        assert(n > 0 && rangeL <= rangeR);
+        int *arr = new int[n];
+        
+        srand(time(NULL));
+        for (int i = 0; i < n; i++) {
+            arr[i] = rand() % (rangeR - rangeL + 1) + rangeL;
+        }
+        return arr;
+    }
 }
 
 
