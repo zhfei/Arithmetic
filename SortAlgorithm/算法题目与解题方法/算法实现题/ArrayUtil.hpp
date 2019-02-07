f093267bb157b420075ba3d59032c023303f41bc
周飞 <1174977961@qq.com>
2019-02-07 23:37:12 +0800

    feature:添加数组生成函数

--- SortAlgorithm/算法题目与解题方法/算法实现题/ArrayUtil.hpp
+++ SortAlgorithm/算法题目与解题方法/算法实现题/ArrayUtil.hpp
@@ -27,6 +27,25 @@ namespace ArrayUtil {
         }
         return arr;
     }
+    
+    int *generateOrderedArray(int n) {
+        assert(n > 0);
+        
+        int *arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = i;
+        }
+        return arr;
+    }
+    
+    template<typename T>
+    void printArray(T arr[], int n) {
+        for (int i = 0; i< n; i++) {
+            cout << arr[i] << " ";
+            cout << endl;
+        }
+        return;
+    }
 }
 
 
