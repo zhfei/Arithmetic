e1cfa2fc6e1e9428e0dc43731ce4aa91c61d39a1
周飞 <1174977961@qq.com>
2019-02-27 23:22:53 +0800

    add: 添加链表相加

--- SortAlgorithm/LeeCode/LeeCode.c
+++ SortAlgorithm/LeeCode/LeeCode.c
@@ -7,6 +7,7 @@
 //
 
 #include "LeeCode.h"
+#include <stdlib.h>
 
 //两数之和等于目标数的 下标数组
 //利用冒泡排序的两个for循环思想
@@ -28,4 +29,56 @@ int* twoSum(int* nums, int numsSize, int target) {
         }
     }
     return 0;
+}
+
+
+//链表两数求和
+//* Definition for singly-linked list.
+typedef struct ListNode {
+  int val;
+  struct ListNode *next;
+}*pListNode,ListNode;
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    if (l1 == NULL) {
+        return l2;
+    }
+    
+    if (l2 == NULL) {
+        return l1;
+    }
+    
+    pListNode pn = (pListNode)malloc(sizeof(ListNode));
+    pListNode header = pn;
+    pListNode tail = header;
+
+    int tmp = 0;
+
+    pListNode pNode1 = l1;
+    pListNode pNode2 = l2;
+
+    while (pNode1 != NULL || pNode2 != NULL) {
+        tail = pn;
+        if (pNode1 == NULL) {
+            pn->val = pNode2->val;
+        } else if (pNode2 == NULL) {
+            pn->val = pNode1->val;
+        } else {
+            int res = pNode1->val + pNode2->val;
+            if (res > 9) {
+                tmp = res/10 ;
+                pn->val = res%10;
+            } else {
+                pn->val = res;
+            }
+        }
+        pn = (pListNode)malloc(sizeof(ListNode));
+        tail->next = pn;
+        
+        pNode1 = l1->next;
+        pNode2 = l2->next;
+    }
+    
+    free(pn);
+    return header;
 }
